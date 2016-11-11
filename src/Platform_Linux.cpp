cc9f354006e5c1099dc28e6f2e94f4b30d2c81a6
sippeangelo <holmberg.simon@gmail.com>
2016-11-11 21:21:31 +0100

    Fixed file mode for Linux EFI variables

--- src/Platform_Linux.cpp
+++ src/Platform_Linux.cpp
@@ -27,7 +27,7 @@ std::pair<std::uint8_t*, std::size_t> Platform::UEFIReadVariable(const std::stri
 
 void Platform::UEFIWriteVariable(const std::string& name, void* buffer, std::size_t size)
 {
-    if (efi_set_variable(EFI_GLOBAL_GUID, name.c_str(), static_cast<std::uint8_t*>(buffer), size, EFI_VARIABLE_RUNTIME_ACCESS | EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_NON_VOLATILE, 0600) != 0) {
+    if (efi_set_variable(EFI_GLOBAL_GUID, name.c_str(), static_cast<std::uint8_t*>(buffer), size, EFI_VARIABLE_RUNTIME_ACCESS | EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_NON_VOLATILE, 0644) != 0) {
         throw std::runtime_error("Failed to write UEFI variable: BootNext");
     }
 }
